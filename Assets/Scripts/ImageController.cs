87a03dcd9516b3b747ec7f2e070d4f79f38af5a1
Dan Corman <dan.corman@gmail.com>
2016-05-21 21:28:30 -0500

    Added images and image controller, stubbed out routing for running JS script

---
+++ Assets/Scripts/ImageController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ImageController : GameController {
+
+	
+	public Image Animals;
+	// Use this for initialization
+	void Start () {
+		myState = States.splash;
+		previousStates = new Stack<States> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Backspace) && previousStates.Count > 0) {
+			myState = previousStates.Pop();
+		}
+		router ();
+		renderImages ();
+	}
+
+	private void renderImages () {
+		if (myState != States.splash) {
+			Animals.enabled = false;
+		} else {
+			Animals.enabled = true;
+		}
+	}
+}
